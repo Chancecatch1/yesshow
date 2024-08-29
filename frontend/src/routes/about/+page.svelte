a7b990377441fdeae7e1ed080f8f5b926c2df633
chancecatch1 <chancecatch1@gmail.com>
2024-08-29 11:12:31 -0600

    install lightgbm

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -4,12 +4,17 @@
 	<div class="md:w-2/3 w-full">
 		<div class="text-2xl font-semibold mb-2">
 			안녕하세요. 우리는
-			<span class="text-[#696969]"> </span>
-			<span class="text-[#696969]"> Book so. Inc. </span>
+			<span class="text-[#696969]"> 북쏘 </span>
+			<span class="text-2xl font-semibold mb-2"> 입니다. </span>
 		</div>
-		<div
-			class="text-black text-[3vw] md:text-xl font-normal font-['Oxygen'] leading-[1.5em] mt-4"
-		></div>
+		<span class="text-[#696969]"> YESSHOW는 특정 공연에 대해서 얼마나 그리고 언제</span>
+		<span class="text-[#696969]"> 취소가 발생할지 예측합니다.</span> <br />
+		<span class="text-[#696969]">
+			공연의 주최측은 선제적으로 취소에 대처하고, 대기 관객들에게 언제 취소가 발생할지 가능성을
+			제안함으로써 보다 효과적으로 좌석을 운영할 수 있습니다. 더불어 Kopis 빅데이터(2019년 7월 1일 ~
+			2023년 12월 31일) 취소 분석을 통한 인사이트를 제공함으로써 추후 공연 설계시 도움이 될 수
+			있도록 지원합니다.</span
+		>
 	</div>
 </div>
 
